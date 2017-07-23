27b87933afe054c3e410f2174c48cfdf867a9df2
Ning Zhang <yaseru2003@gmail.com>
2017-07-23 11:46:25 -0400

    Map to World Coordinates
    feat: convert rover-centric coordinates into world coordinates.
      "rotate_pix" function implement rotation on pixels; "translate_pix" function adds rover's (x, y) position w.r.t. world coordinates to rotated pixels.
    
      "pix_to_world" is a wrap-up function to rotate and translate pixels. furthermore it clips resulted pixels that are beyond the world map grid.

--- project-rover-on-mars/practice_codes.py
+++ project-rover-on-mars/practice_codes.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 #import numpy as np
 import numpy as np
 import cv2
-from support_functions import color_thresh, perspect_transform, rover_coords
+from support_functions import color_thresh, perspect_transform, rover_coords, pix_to_world
 
 # Uncomment the next line for use in a Jupyter notebook
 #%matplotlib inline
@@ -87,10 +87,58 @@ colorsel = color_thresh(warped, rgb_thresh=(160, 160, 160))
 # Extract x and y positions of navigable terrain pixels
 # and convert to rover coordinates
 xpix, ypix = rover_coords(colorsel)
-
+"""
 # Plot the map in rover-centric coords
 fig = plt.figure(figsize=(5, 7.5))
 plt.plot(xpix, ypix, '.')
 plt.ylim(-160, 160)
 plt.xlim(0, 160)
 plt.title('Rover-Centric Map', fontsize=20)
+"""
+#Map to World Coordinates
+"""
+So far, the coordinates have been converted into rover-centric coordinates,
+ in which rover is located at (0, 0). Next, the rover's position needs to be 
+ embedded into world map coordinates. To do so, the rover-centric coordinates 
+ need to be rotated and translated.
+
+"""
+# Rover yaw values will come as floats from 0 to 360
+# Generate a random value in this range
+# Note: you need to convert this to radians
+    # before adding to pixel_angles
+rover_yaw = np.random.random(1)*360
+
+# Generate a random rover position in world coords
+# Position values will range from 20 to 180 to 
+# avoid the edges in a 200 x 200 pixel world
+rover_xpos = np.random.random(1)*160 + 20
+rover_ypos = np.random.random(1)*160 + 20
+
+# Generate 200 x 200 pixel worldmap
+worldmap = np.zeros((200, 200))
+scale = 10
+# Get navigable pixel positions in world coords
+x_world, y_world = pix_to_world(xpix, ypix, rover_xpos, 
+                                rover_ypos, rover_yaw, 
+                                worldmap.shape[0], scale)
+# Add pixel positions to worldmap
+worldmap[y_world, x_world] += 1
+print('Xpos =', rover_xpos, 'Ypos =', rover_ypos, 'Yaw =', rover_yaw)
+# Plot the map in rover-centric coords
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 7))
+f.tight_layout()
+ax1.plot(xpix, ypix, '.')
+ax1.set_title('Rover Space', fontsize=40)
+ax1.set_ylim(-160, 160)
+ax1.set_xlim(0, 160)
+ax1.tick_params(labelsize=20)
+
+ax2.imshow(worldmap, cmap='gray')
+ax2.set_title('World Space', fontsize=40)
+ax2.set_ylim(0, 200)
+ax2.tick_params(labelsize=20)
+ax2.set_xlim(0, 200)
+
+plt.subplots_adjust(left=0.1, right=1, top=0.9, bottom=0.1)
--- project-rover-on-mars/support_functions.py
+++ project-rover-on-mars/support_functions.py
@@ -43,3 +43,36 @@ def rover_coords(binary_img):
     x_pixel = -(ypos - binary_img.shape[0]).astype(np.float)
     y_pixel = -(xpos - binary_img.shape[1]/2 ).astype(np.float)
     return x_pixel, y_pixel
+
+# Define a function to map rover space pixels to world space
+# rotation matrix
+# [cos(theta), -sin(theta)]
+# [sin(theta), cos(theta)]
+def rotate_pix(xpix, ypix, yaw):
+    # Convert yaw to radians
+    yaw_rad = yaw * np.pi / 180
+    xpix_rotated = (xpix * np.cos(yaw_rad)) - (ypix * np.sin(yaw_rad))
+                            
+    ypix_rotated = (xpix * np.sin(yaw_rad)) + (ypix * np.cos(yaw_rad))
+    # Return the result  
+    return xpix_rotated, ypix_rotated
+
+def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale): 
+    # Apply a scaling and a translation
+    xpix_translated = (xpix_rot / scale) + xpos
+    ypix_translated = (ypix_rot / scale) + ypos
+    # Return the result  
+    return xpix_translated, ypix_translated
+
+# Define a function to apply rotation and translation (and clipping)
+# Once you define the two functions above this function should work
+def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
+    # Apply rotation
+    xpix_rot, ypix_rot = rotate_pix(xpix, ypix, yaw)
+    # Apply translation
+    xpix_tran, ypix_tran = translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale)
+    # Perform rotation, translation and clipping all at once
+    x_pix_world = np.clip(np.int_(xpix_tran), 0, world_size - 1)
+    y_pix_world = np.clip(np.int_(ypix_tran), 0, world_size - 1)
+    # Return the result
+    return x_pix_world, y_pix_world
